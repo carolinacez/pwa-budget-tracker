1998c8c99821a981d926798fb8cef0ec200d841b
carolina <carolinaccez@gmail.com>
2020-10-10 14:50:44 -0400

    deploy2

--- public/js/idb.js
+++ public/js/idb.js
@@ -35,7 +35,7 @@ function uploadBudget() {
     const getAll = budgetObjectStore.getAll();
     getAll.onsuccess = function() {
         if(getAll.result.length > 0) {
-            fetch('/api/', {
+            fetch('/api/transaction', {
                 method: 'POST',
                 body: JSON.stringify(getAll.result),
                 headers: {
