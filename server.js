6e5634f349218d1fb7103750d8f43e4f8cc1fed7
carolina <carolinaccez@gmail.com>
2020-10-10 20:23:23 -0400

    deploy5

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const mongoose = require("mongoose");
 const compression = require("compression");
 
 const PORT = process.env.PORT || 3001;
-// const MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/budget";
+const MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/budget";
 
 const app = express();
 
@@ -16,15 +16,15 @@ app.use(express.json());
 
 app.use(express.static("public"));
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/budget', {
-  useFindAndModify: false,
-  useNewUrlParser: true,
-  useUnifiedTopology: true
-});
-// mongoose.connect(MONGODB_URI, {
+// mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/budget', {
+//   useFindAndModify: false,
 //   useNewUrlParser: true,
-//   useFindAndModify: false
+//   useUnifiedTopology: true
 // });
+mongoose.connect(MONGODB_URI, {
+  useNewUrlParser: true,
+  useFindAndModify: false
+});
 
 // routes
 app.use(require("./routes/api.js"));
